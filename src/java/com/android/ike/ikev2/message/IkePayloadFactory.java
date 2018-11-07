ecca27dfed9d5085871570ea07c5bfbfc40e2838
evitayan <evitayan@google.com>
2018-11-07 11:16:34 -0800

    Create Auth Payload
    This commit:
    - Create abstract class of IkeAuthPayload
    - Create IkeAuthPskPayload
    - Fix bug in checking unrecognized Attribute
    
    Bug: 119223276
    Test: FrameworksIkeTests
    Change-Id: Ief56d72d3fd54d1e4476cb4031177a498bbf7632

--- src/java/com/android/ike/ikev2/message/IkePayloadFactory.java
+++ src/java/com/android/ike/ikev2/message/IkePayloadFactory.java
@@ -65,6 +65,8 @@ final class IkePayloadFactory {
                             return new IkeIdPayload(isCritical, payloadBody, false);
                         case IkePayload.PAYLOAD_TYPE_CERT:
                             return new IkeCertPayload(isCritical, payloadBody);
+                        case IkePayload.PAYLOAD_TYPE_AUTH:
+                            return IkeAuthPayload.getIkeAuthPayload(isCritical, payloadBody);
                         case IkePayload.PAYLOAD_TYPE_NONCE:
                             return new IkeNoncePayload(isCritical, payloadBody);
                         case IkePayload.PAYLOAD_TYPE_NOTIFY:
