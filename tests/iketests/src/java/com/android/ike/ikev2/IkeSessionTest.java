0cf6592278e43c0757622ae1796ec143a22fb63c
Cody Kesting <ckesting@google.com>
2019-07-08 10:21:07 -0700

    Fix potential NPE in ikev2 unit tests.
    Bug: 137008522
    Test: atest FrameworksIkeTests
    Change-Id: Id679063db06540ed8737894fdf3311d2c8339af6

--- tests/iketests/src/java/com/android/ike/ikev2/IkeSessionTest.java
+++ tests/iketests/src/java/com/android/ike/ikev2/IkeSessionTest.java
@@ -48,7 +48,7 @@ public final class IkeSessionTest {
 
     @Before
     public void setUp() throws Exception {
-        if (Looper.myLooper() == null) Looper.myLooper().prepare();
+        if (Looper.myLooper() == null) Looper.prepare();
 
         mMockIpSecTestUtils = MockIpSecTestUtils.setUpMockIpSec();
         mIpSecManager = mMockIpSecTestUtils.getIpSecManager();
