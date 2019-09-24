f955dbc56431a4c985c1d3341c665ade1c6ea8f1
evitayan <evitayan@google.com>
2019-09-24 12:56:31 -0700

    Validate received certificates
    Bug: 122685769
    Test: atest FrameworksIkeTests(new tests passed)
    Change-Id: I7574fdfe6b8581702632bf2df094794f9d51a526

--- src/java/com/android/ike/ikev2/message/IkeCertPayload.java
+++ src/java/com/android/ike/ikev2/message/IkeCertPayload.java
@@ -17,13 +17,29 @@
 package com.android.ike.ikev2.message;
 
 import android.annotation.IntDef;
+import android.annotation.Nullable;
 
 import com.android.ike.ikev2.exceptions.AuthenticationFailedException;
 import com.android.ike.ikev2.exceptions.IkeProtocolException;
 
+import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.nio.ByteBuffer;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.ProviderException;
+import java.security.cert.CertificateException;
+import java.security.cert.TrustAnchor;
+import java.security.cert.X509CRL;
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.Set;
+
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * IkeCertPayload is an abstract class that represents the common information for all Certificate
@@ -38,6 +54,10 @@ import java.nio.ByteBuffer;
 public abstract class IkeCertPayload extends IkePayload {
     // Length of certificate encoding type field in octets.
     private static final int CERT_ENCODING_LEN = 1;
+
+    private static final String KEY_STORE_TYPE_PKCS12 = "PKCS12";
+    private static final String CERT_PATH_ALGO_PKIX = "PKIX";
+    private static final String CERT_AUTH_TYPE_RSA = "RSA";
 
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({
@@ -77,6 +97,75 @@ public abstract class IkeCertPayload extends IkePayload {
                         "CERTIFICATE_ENCODING_X509_CERT_HASH_URL decoding is unsupported");
             default:
                 throw new AuthenticationFailedException("Unrecognized certificate encoding type.");
+        }
+    }
+
+    /**
+     * Validate an end certificate against the received chain and trust anchors.
+     *
+     * <p>Validation is done by checking if there is a valid certificate path from end certificate
+     * to provided trust anchors.
+     *
+     * <p>TrustManager implementation used in this method MUST conforms RFC 4158 and RFC 5280. As
+     * indicated in RFC 4158, Key Identifiers(KIDs) are not required to match during certification
+     * path validation and cannot be used to eliminate certificates.
+     *
+     * <p>Validation will fail if any certficate in the certificate chain is using RSA public key
+     * whose RSA modulus is smaller than 1024 bits.
+     *
+     * @param endCert the end certificate that will be used to verify AUTH payload
+     * @param certList all the received certificates (include the end certificate)
+     * @param crlList the certificate revocation lists
+     * @param trustAnchorSet the certificate authority set to validate the end certificate
+     * @throws AuthenticationFailedException if there is no valid certificate path
+     */
+    public static void validateCertificates(
+            X509Certificate endCert,
+            List<X509Certificate> certList,
+            @Nullable List<X509CRL> crlList,
+            Set<TrustAnchor> trustAnchorSet)
+            throws AuthenticationFailedException {
+        try {
+            // TODO: b/122676944 Support CRL checking
+
+            // Create a new keyStore with all trusted anchors
+            KeyStore keyStore =
+                    KeyStore.getInstance(KEY_STORE_TYPE_PKCS12, IkeMessage.getSecurityProvider());
+            keyStore.load(null);
+            for (TrustAnchor t : trustAnchorSet) {
+                X509Certificate trustedCert = t.getTrustedCert();
+                String alias =
+                        trustedCert.getSubjectX500Principal().getName() + trustedCert.hashCode();
+                keyStore.setCertificateEntry(alias, trustedCert);
+            }
+
+            // Build X509TrustManager with all keystore
+            TrustManagerFactory tmFactory =
+                    TrustManagerFactory.getInstance(
+                            CERT_PATH_ALGO_PKIX, IkeMessage.getTrustManagerProvider());
+            tmFactory.init(keyStore);
+
+            X509TrustManager trustManager = null;
+            for (TrustManager tm : tmFactory.getTrustManagers()) {
+                if (tm instanceof X509TrustManager) {
+                    trustManager = (X509TrustManager) tm;
+                }
+            }
+            if (trustManager == null) {
+                throw new ProviderException(
+                        "X509TrustManager is not supported by "
+                                + IkeMessage.getTrustManagerProvider().getName());
+            }
+
+            // Build and validate certificate path
+            trustManager.checkServerTrusted(
+                    certList.toArray(new X509Certificate[certList.size()]), CERT_AUTH_TYPE_RSA);
+        } catch (NoSuchAlgorithmException e) {
+            throw new ProviderException("Algorithm is not supported by the provider", e);
+        } catch (IOException | KeyStoreException e) {
+            throw new IllegalStateException(e);
+        } catch (CertificateException e) {
+            throw new AuthenticationFailedException(e);
         }
     }
 }
