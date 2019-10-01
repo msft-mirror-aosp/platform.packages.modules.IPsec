d824ae4791450488ab87ce8d0c7b8858410045e5
evitayan <evitayan@google.com>
2019-10-01 15:40:31 -0700

    Support configuring internal address requests
    Bug: 140644654
    Test: atest FrameworksIkeTests(new tests added)
    Change-Id: I124575be73dc73381fe38c72beceb3176777807c

--- src/java/com/android/ike/ikev2/TunnelModeChildSessionOptions.java
+++ src/java/com/android/ike/ikev2/TunnelModeChildSessionOptions.java
@@ -16,25 +16,56 @@
 
 package com.android.ike.ikev2;
 
+import static android.system.OsConstants.AF_INET;
+import static android.system.OsConstants.AF_INET6;
+
 import android.annotation.NonNull;
+import android.net.LinkAddress;
+
+import com.android.ike.ikev2.message.IkeConfigPayload.ConfigAttribute;
+import com.android.ike.ikev2.message.IkeConfigPayload.ConfigAttributeIpv4Address;
+import com.android.ike.ikev2.message.IkeConfigPayload.ConfigAttributeIpv4Netmask;
+import com.android.ike.ikev2.message.IkeConfigPayload.ConfigAttributeIpv6Address;
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * This class contains all user provided configuration options for negotiating a tunnel mode Child
  * Session.
  */
 public final class TunnelModeChildSessionOptions extends ChildSessionOptions {
+    private final ConfigAttribute[] mConfigRequests;
+
     private TunnelModeChildSessionOptions(
             IkeTrafficSelector[] localTs,
             IkeTrafficSelector[] remoteTs,
-            ChildSaProposal[] proposals) {
+            ChildSaProposal[] proposals,
+            ConfigAttribute[] configRequests) {
         super(localTs, remoteTs, proposals, false /*isTransport*/);
+        mConfigRequests = configRequests;
+    }
+
+    /** Package private */
+    ConfigAttribute[] getConfigurationRequests() {
+        return mConfigRequests;
     }
 
     /** This class can be used to incrementally construct a TunnelModeChildSessionOptions. */
     public static final class Builder extends ChildSessionOptions.Builder {
-        /** Create a Builder for negotiating a tunnel mode Child Session. */
+        private static final int IPv4_DEFAULT_PREFIX_LEN = 32;
+
+        private boolean mHasIp4AddressRequest;
+        private List<ConfigAttribute> mConfigRequestList;
+
+        /** Create a Builder for negotiating a transport mode Child Session. */
         public Builder() {
             super();
+            mHasIp4AddressRequest = false;
+            mConfigRequestList = new LinkedList<>();
         }
 
         /**
@@ -50,6 +81,56 @@ public final class TunnelModeChildSessionOptions extends ChildSessionOptions {
         }
 
         /**
+         * Adds internal address requests to TunnelModeChildSessionOptions being built.
+         *
+         * @param addressFamily the address family. Only OsConstants.AF_INET and
+         *     OsConstants.AF_INET6 are allowed.
+         * @param numOfRequest the number of requests for this type of address.
+         * @return Builder this, to facilitate chaining.
+         */
+        public Builder addInternalAddressRequest(int addressFamily, int numOfRequest) {
+            if (addressFamily == AF_INET) {
+                mHasIp4AddressRequest = true;
+                for (int i = 0; i < numOfRequest; i++) {
+                    mConfigRequestList.add(new ConfigAttributeIpv4Address());
+                }
+                return this;
+            } else if (addressFamily == AF_INET6) {
+                for (int i = 0; i < numOfRequest; i++) {
+                    mConfigRequestList.add(new ConfigAttributeIpv6Address());
+                }
+                return this;
+            } else {
+                throw new IllegalArgumentException("Invalid address family: " + addressFamily);
+            }
+        }
+
+        /**
+         * Adds specific internal address request to TunnelModeChildSessionOptions being built.
+         *
+         * @param address the requested address.
+         * @param prefixLen length of the InetAddress prefix. When requesting an IPv4 address,
+         *     prefixLen MUST be 32.
+         * @return Builder this, to facilitate chaining.
+         */
+        public Builder addInternalAddressRequest(@NonNull InetAddress address, int prefixLen) {
+            if (address instanceof Inet4Address) {
+                if (prefixLen != IPv4_DEFAULT_PREFIX_LEN) {
+                    throw new IllegalArgumentException("Invalid IPv4 prefix length: " + prefixLen);
+                }
+                mHasIp4AddressRequest = true;
+                mConfigRequestList.add(new ConfigAttributeIpv4Address((Inet4Address) address));
+                return this;
+            } else if (address instanceof Inet6Address) {
+                mConfigRequestList.add(
+                        new ConfigAttributeIpv6Address(new LinkAddress(address, prefixLen)));
+                return this;
+            } else {
+                throw new IllegalArgumentException("Invalid address " + address);
+            }
+        }
+
+        /**
          * Validates, builds and returns the TunnelModeChildSessionOptions.
          *
          * @return the validated TunnelModeChildSessionOptions.
@@ -58,10 +139,15 @@ public final class TunnelModeChildSessionOptions extends ChildSessionOptions {
         public TunnelModeChildSessionOptions build() {
             validateOrThrow();
 
+            if (mHasIp4AddressRequest) {
+                mConfigRequestList.add(new ConfigAttributeIpv4Netmask());
+            }
+
             return new TunnelModeChildSessionOptions(
                     mLocalTsList.toArray(new IkeTrafficSelector[mLocalTsList.size()]),
                     mRemoteTsList.toArray(new IkeTrafficSelector[mRemoteTsList.size()]),
-                    mSaProposalList.toArray(new ChildSaProposal[mSaProposalList.size()]));
+                    mSaProposalList.toArray(new ChildSaProposal[mSaProposalList.size()]),
+                    mConfigRequestList.toArray(new ConfigAttribute[mConfigRequestList.size()]));
         }
     }
 
