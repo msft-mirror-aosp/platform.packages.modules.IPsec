994ddb890597c9229d637aecf237a5b3db41e680
evitayan <evitayan@google.com>
2020-01-29 18:55:44 -0800

    Fix all missing implementations in IkeSessionConfiguration
    Pass IKE connection info, remote vendor IDs and enabled IKE
    extensions to IkeSessionConfiguration and allow user to retrieve
    them.
    
    Bug: 142743562
    Test: FrameworksIkeTests(new tests added), FrameworksNetTests
    Change-Id: Ife1d43b6968428677ec3cf352df06bd476abbb9b

--- tests/iketests/src/java/android/net/ipsec/ike/IkeSessionConfigurationTest.java
+++ tests/iketests/src/java/android/net/ipsec/ike/IkeSessionConfigurationTest.java
@@ -16,7 +16,10 @@
 
 package android.net.ipsec.ike;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 
@@ -24,6 +27,7 @@ import android.net.InetAddresses;
 
 import com.android.internal.net.ipsec.ike.message.IkeConfigPayload;
 import com.android.internal.net.ipsec.ike.message.IkeConfigPayload.ConfigAttribute;
+import com.android.internal.net.ipsec.ike.message.IkeConfigPayload.ConfigAttributeAppVersion;
 import com.android.internal.net.ipsec.ike.message.IkeConfigPayload.ConfigAttributeIpv4Pcscf;
 import com.android.internal.net.ipsec.ike.message.IkeConfigPayload.ConfigAttributeIpv6Pcscf;
 
@@ -31,11 +35,14 @@ import org.junit.Test;
 
 import java.net.Inet4Address;
 import java.net.Inet6Address;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public final class IkeSessionConfigurationTest {
+    private static final String REMOTE_APP_VERSION = "Test IKE Server 1.0";
+
     private static final Inet4Address PCSCF_IPV4_ADDRESS =
             (Inet4Address) (InetAddresses.parseNumericAddress("192.0.2.100"));
     private static final Inet6Address PCSCF_IPV6_ADDRESS =
@@ -44,29 +51,75 @@ public final class IkeSessionConfigurationTest {
     private static final IkeSessionConnectionInfo IKE_CONNECT_INFO =
             mock(IkeSessionConnectionInfo.class);
 
-    @Test
-    public void testBuildWithoutPcscfAddresses() {
-        IkeSessionConfiguration config =
-                new IkeSessionConfiguration(IKE_CONNECT_INFO, null /*configPayload*/);
+    private static final List<byte[]> REMOTE_VENDOR_IDS;
+    private static final List<Integer> ENABLED_EXTENSIONS;
+
+    static {
+        REMOTE_VENDOR_IDS = new ArrayList<>();
+        REMOTE_VENDOR_IDS.add("vendorIdA".getBytes());
+        REMOTE_VENDOR_IDS.add("vendorIdB".getBytes());
+
+        ENABLED_EXTENSIONS = new ArrayList<>();
+        ENABLED_EXTENSIONS.add(IkeSessionConfiguration.EXTENSION_TYPE_FRAGMENTATION);
+    }
+
+    private void verifyBuildCommon(IkeSessionConfiguration config) {
+        // Verify Vendor IDs
+        List<byte[]> remoteVendorIds = config.getRemoteVendorIds();
+        assertEquals(REMOTE_VENDOR_IDS.size(), remoteVendorIds.size());
+        for (int i = 0; i < REMOTE_VENDOR_IDS.size(); i++) {
+            assertArrayEquals(REMOTE_VENDOR_IDS.get(i), remoteVendorIds.get(i));
+        }
+
+        // Verify enabled extensions
+        assertTrue(
+                config.isIkeExtensionEnabled(IkeSessionConfiguration.EXTENSION_TYPE_FRAGMENTATION));
+        assertFalse(config.isIkeExtensionEnabled(IkeSessionConfiguration.EXTENSION_TYPE_MOBIKE));
+
+        // Verify IkeSessionConnectionInfo
         assertEquals(IKE_CONNECT_INFO, config.getIkeSessionConnectionInfo());
     }
 
     @Test
-    public void testBuildWithPcscfAddresses() {
+    public void testBuildWithoutConfigPayload() {
+        IkeSessionConfiguration config =
+                new IkeSessionConfiguration(
+                        IKE_CONNECT_INFO,
+                        null /*configPayload*/,
+                        REMOTE_VENDOR_IDS,
+                        ENABLED_EXTENSIONS);
+        verifyBuildCommon(config);
+    }
+
+    @Test
+    public void testBuildWithConfigPayload() {
         List<ConfigAttribute> attributeList = new LinkedList<>();
         attributeList.add(new ConfigAttributeIpv4Pcscf(PCSCF_IPV4_ADDRESS));
         attributeList.add(new ConfigAttributeIpv6Pcscf(PCSCF_IPV6_ADDRESS));
+        attributeList.add(new ConfigAttributeAppVersion(REMOTE_APP_VERSION));
 
         IkeConfigPayload configPayload = new IkeConfigPayload(true /*isReply*/, attributeList);
 
         IkeSessionConfiguration config =
-                new IkeSessionConfiguration(IKE_CONNECT_INFO, configPayload);
+                new IkeSessionConfiguration(
+                        IKE_CONNECT_INFO, configPayload, REMOTE_VENDOR_IDS, ENABLED_EXTENSIONS);
 
-        assertEquals(IKE_CONNECT_INFO, config.getIkeSessionConnectionInfo());
+        verifyBuildCommon(config);
         assertEquals(
                 Arrays.asList(PCSCF_IPV4_ADDRESS, PCSCF_IPV6_ADDRESS), config.getPcscfServers());
+        assertEquals(REMOTE_APP_VERSION, config.getRemoteApplicationVersion());
     }
 
-    // TODO(b/150466460): Test that constructing IkeSessionConfiguration without
-    // IkeSessionConnectionInfo will fail
+    @Test
+    public void testBuildWithNullValueConnectionInfo() {
+        try {
+            new IkeSessionConfiguration(
+                    null /*ikeConnInfo*/,
+                    null /*configPayload*/,
+                    REMOTE_VENDOR_IDS,
+                    ENABLED_EXTENSIONS);
+            fail("Expected to fail due to null value ikeConnInfo");
+        } catch (NullPointerException expected) {
+        }
+    }
 }
