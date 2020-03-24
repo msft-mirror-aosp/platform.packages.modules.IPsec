274a73d4d20cd0e3f9d7f58cf9523e29cc83644a
Benedict Wong <benedictwong@google.com>
2020-03-23 20:39:22 -0700

    Expose 3072 and 4096 MODP groups
    This exposes the 3072 and 4096 MODP groups for use with VPNs.
    
    Bug: 158006065
    Test: FrameworksIkeTests passing
    Change-Id: I87ac9549203812f5ac541cd1572fd1764b53bdfb

--- src/java/android/net/ipsec/ike/SaProposal.java
+++ src/java/android/net/ipsec/ike/SaProposal.java
@@ -182,7 +182,14 @@ public abstract class SaProposal {
 
     /** @hide */
     @Retention(RetentionPolicy.SOURCE)
-    @IntDef({DH_GROUP_NONE, DH_GROUP_1024_BIT_MODP, DH_GROUP_1536_BIT_MODP, DH_GROUP_2048_BIT_MODP})
+    @IntDef({
+        DH_GROUP_NONE,
+        DH_GROUP_1024_BIT_MODP,
+        DH_GROUP_1536_BIT_MODP,
+        DH_GROUP_2048_BIT_MODP,
+        DH_GROUP_3072_BIT_MODP,
+        DH_GROUP_4096_BIT_MODP
+    })
     public @interface DhGroup {}
 
     /** None Diffie-Hellman Group. */
@@ -193,9 +200,9 @@ public abstract class SaProposal {
     public static final int DH_GROUP_1536_BIT_MODP = 5;
     /** 2048-bit MODP Diffie-Hellman Group. */
     public static final int DH_GROUP_2048_BIT_MODP = 14;
-    /** 3072-bit MODP Diffie-Hellman Group. @hide */
+    /** 3072-bit MODP Diffie-Hellman Group. */
     public static final int DH_GROUP_3072_BIT_MODP = 15;
-    /** 4096-bit MODP Diffie-Hellman Group. @hide */
+    /** 4096-bit MODP Diffie-Hellman Group. */
     public static final int DH_GROUP_4096_BIT_MODP = 16;
 
     private static final SparseArray<String> SUPPORTED_DH_GROUP_TO_STR;
