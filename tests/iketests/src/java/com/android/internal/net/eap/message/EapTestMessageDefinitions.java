111769626948cb165a1111a8189bb3d5a1be6ee7
Rayan Osseiran <rayano@google.com>
2020-08-19 23:36:37 +0000

    Follow-up of AOSP/1393303
    - Follow-up for non-functional nits.
    - Updates message lengths to a more realistic range.
    Tested fragments are now 64 bytes (up from 5 bytes).
    
    Test: atest FrameworksIkeTests
    Change-Id: I276ca61e093654bde6a05390215a172a6e275b37

--- tests/iketests/src/java/com/android/internal/net/eap/message/EapTestMessageDefinitions.java
+++ tests/iketests/src/java/com/android/internal/net/eap/message/EapTestMessageDefinitions.java
@@ -334,10 +334,16 @@ public class EapTestMessageDefinitions {
                             + "32020000"); // EAP-AKA' | Authentication Reject | 2B padding
 
     // EAP-TTLS test vectors
-    public static final String EAP_TTLS_DUMMY_DATA = "1701000006";
-    public static final String EAP_TTLS_DUMMY_DATA_ASSEMBLED_FRAGMENT = "16030100a5010000a1";
-    public static final String EAP_TTLS_DUMMY_DATA_INITIAL_FRAGMENT = "16030100a5";
-    public static final String EAP_TTLS_DUMMY_DATA_FINAL_FRAGMENT = "010000a1";
+    public static final String EAP_TTLS_DUMMY_DATA =
+            "17010160301000E050010a516030100a010000a151603010036313233343"
+                    + "5363738393031323334003A31316030100a55a51603320500000100a516030100a50";
+    public static final String EAP_TTLS_DUMMY_DATA_INITIAL_FRAGMENT =
+            "36313233343536373839303132333435030100a010000a151603010036313"
+                    + "2333435363738393031323334003A31316030100a55a51603320500000100a51603";
+    public static final String EAP_TTLS_DUMMY_DATA_FINAL_FRAGMENT =
+            "010000a10E050010320200000000000000000000";
+    public static final String EAP_TTLS_DUMMY_DATA_ASSEMBLED_FRAGMENT =
+            EAP_TTLS_DUMMY_DATA_INITIAL_FRAGMENT + EAP_TTLS_DUMMY_DATA_FINAL_FRAGMENT;
 
     public static final byte[] EAP_TTLS_DUMMY_DATA_BYTES =
             hexStringToByteArray(EAP_TTLS_DUMMY_DATA);
