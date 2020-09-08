5afef4b10488e50fcb2ce559a4855e3fd58640e1
Cody Kesting <ckesting@google.com>
2020-09-08 16:45:44 -0700

    Allow 3GPP-specific payload configuration for IKEv2.
    This CL updates the IKEv2 Library to support 3GPP-specific payloads for
    IWLAN. Ike3gppExtension is defined to encapsulate an Ike3gppCallback
    (for notifications about receiving supported 3GPP-payloads) and an
    Ike3gppParams (for configuring the IKEv2's use of 3GPP-payloads).
    
    Bug: 169856575
    Test: atest FrameworksIkeTests
    Change-Id: I4cbe9a6664a4bb172e7166aecdfbc5fb8efcb36c

---
+++ src/java/android/net/ipsec/ike/ike3gpp/Ike3gppInfo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.ipsec.ike.ike3gpp;
+
+import android.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Abstract class representing a 3GPP-specific payload.
+ *
+ * @see 3GPP ETSI TS 24.302: Access to the 3GPP Evolved Packet Core (EPC) via non-3GPP access
+ *     networks
+ * @hide
+ */
+public abstract class Ike3gppInfo {
+    /** @hide */
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef()
+    public @interface InfoType {}
+
+    /** Returns the enum that this Ike3gppInfo represents. */
+    @InfoType
+    public abstract int getInfoType();
+}
